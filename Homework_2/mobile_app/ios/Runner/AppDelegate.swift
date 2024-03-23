7ef21c32df6029d55a109743863a5316a0128e79
Rumeghea Leonard <leonard.rumeghea@gmail.com>
2024-03-23 19:18:13 +0200

    HM-2: Build on iOS

--- Homework_2/mobile_app/ios/Runner/AppDelegate.swift
+++ Homework_2/mobile_app/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAi4_xNCIhmQTmUL4LEJGM4B7TW44T2Ylc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
